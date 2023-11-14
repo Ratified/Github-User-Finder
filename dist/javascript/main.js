0d4cb7ba08bd24f6b8b8f6e9324fa08513e521bb
RatifiedGeorge <kamungegeorge42@gmail.com>
2023-11-14 08:08:38 +0300

    Finder

---
+++ dist/javascript/main.js
@@ -0,0 +1,16 @@
+"use strict";
+const userInput = document.getElementById('usernameInput');
+const submitBtn = document.getElementById('submitBtn');
+const img = document.getElementById('img');
+const profileUsername = document.getElementById('login');
+const profileName = document.getElementById('name');
+const dateJoined = document.getElementById('date_joined');
+const profileBio = document.getElementById('bio');
+const profileRepos = document.getElementById('public_repos');
+const profileFollowers = document.getElementById('followers');
+const profileFollowing = document.getElementById('following');
+const profileLocation = document.getElementById('location');
+const profileLink = document.getElementById('link');
+const twitterProfile = document.getElementById('twitter_username');
+const profileCompany = document.getElementById('company');
+console.log(userInput, submitBtn, img, profileUsername, profileName, dateJoined, profileBio, profileRepos, profileFollowers, profileFollowing, profileLocation, profileLink, twitterProfile, profileCompany);
